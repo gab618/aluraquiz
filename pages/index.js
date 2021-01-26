1363b595581188e365b7bf1ac6ab453220fd36df
Gabriel <knuckles4x@gmail.com>
2021-01-25 23:49:08 -0300

    esboco com widget

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,116 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import db from "../db.json";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+    padding: 0;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const theme = db.theme;
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4caf50;
+  background-color: #1c1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1,
+  h2,
+  h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1;
+  }
+`;
+
+Widget.Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme }) => theme.colors.primary};
+
+  * {
+    margin: 0;
+  }
+`;
+
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <>
+      <GlobalStyle />
+      <BackgroundImage>
+        <QuizContainer>
+          <Widget>
+            <Widget.Header>
+              <h1>É o Blindas</h1>
+            </Widget.Header>
+            <Widget.Content>
+              <p>vai brincando...</p>
+            </Widget.Content>
+          </Widget>
+        </QuizContainer>
+      </BackgroundImage>
+    </>
+  );
 }
